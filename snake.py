7947ca3acd24d0d8a008b6af00af6ab6aa48d03f
saratiedt <saracristinetiedt@gmail.com>
2021-03-16 20:17:08 -0300

    adicionado movimento

--- snake.py
+++ snake.py
@@ -1,8 +1,8 @@
 import pygame
-pygame.init()
-tela=pygame.display.set_mode((800,600))
+import time
+import random
 
-pygame.display.set_caption('Jogo da cobrinha')
+pygame.init()
 
 azul=(0,0,255)
 verde=(0,255,0)
@@ -10,15 +10,33 @@ branco = (255, 255, 255)
 preto = (0, 0, 0)
 vermelho=(255,0,0)
 
+larguraTela = 800
+alturaTela  = 600
+
+tela=pygame.display.set_mode((larguraTela, alturaTela))
+
+pygame.display.set_caption('Jogo da cobrinha')
+
+
+
 fimDeJogo=False
 
-x1 = 300
-y1 = 300
+x1 = larguraTela/2
+y1 = alturaTela/2
+
+posicaoCobra=10
  
 x1_change = 0       
 y1_change = 0
  
 clock = pygame.time.Clock()
+velocidadeCobra=30
+
+fonte = pygame.font.SysFont(None, 50)
+
+def mensagem(msg,color):
+    mesg = fonte.render(msg, True, color)
+    tela.blit(mesg, [larguraTela/3, alturaTela/2.3])
 
 while not fimDeJogo:
     for event in pygame.event.get():
@@ -26,26 +44,34 @@ while not fimDeJogo:
             fimDeJogo = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x1_change = -10
+                x1_change = -posicaoCobra
                 y1_change = 0
             elif event.key == pygame.K_RIGHT:
-                x1_change = 10
+                x1_change = posicaoCobra
                 y1_change = 0
             elif event.key == pygame.K_UP:
-                y1_change = -10
+                y1_change = -posicaoCobra
                 x1_change = 0
             elif event.key == pygame.K_DOWN:
-                y1_change = 10
+                y1_change = posicaoCobra
                 x1_change = 0
+
+                 
+    if x1 >= larguraTela or x1 < 0 or y1 >= alturaTela or y1 < 0:
+        fimDeJogo = True
  
     x1 += x1_change
     y1 += y1_change
     tela.fill(preto)
-    pygame.draw.rect(tela, verde, [x1, y1, 10, 10])
+    pygame.draw.rect(tela, verde, [x1, y1, posicaoCobra, posicaoCobra])
  
     pygame.display.update()
  
-    clock.tick(30)
+    clock.tick(velocidadeCobra)
  
+mensagem("Você perdeu :(",vermelho)
+pygame.display.update()
+time.sleep(5) 
+
 pygame.quit()
 quit()
